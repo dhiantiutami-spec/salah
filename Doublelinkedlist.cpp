2cd65c5b2846308511aa02d215eafed79c29dfb8
dhiantiutami-spec <dhiantiutami@gmail.com>
2026-04-30 12:04:57 +0700

    isi noMhs di newNode

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -24,7 +24,9 @@ public:
         int nim;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
-        
+
          // Step 1: Allocate memory for new node
         Node *newNode = new Node();
+        
+    }
 }
